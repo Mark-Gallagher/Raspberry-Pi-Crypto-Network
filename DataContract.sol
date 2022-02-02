6697b63e5a2595e91cc787846d6a3d36ef065ab3
Mark-Gallagher <92432812+Mark-Gallagher@users.noreply.github.com>
2022-02-02 12:42:39 +0000

    Add files via upload

---
+++ DataContract.sol
@@ -0,0 +1,16 @@
+pragma solidity ^ 0.5.1;
+
+contract DataContract{
+
+
+    string public constant value = "myValue";
+    bool public myBool = true;
+    int public myInt = 1;
+    uint public myUint = 1;
+    uint256 public myUint256 = 9999;
+    uint8 public myUint8 = 8;
+
+
+
+    
+}
